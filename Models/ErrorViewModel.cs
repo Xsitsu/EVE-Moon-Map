5189a0ce84a870443a0c81b5ceb7c91a91028926
Xsitsu <jacoblocke21@live.com>
2018-12-03 13:41:22 -0800

    Initialized new dotnet MVC project.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EVE_Moon_Map.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

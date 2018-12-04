b993900deda73fe4799bac96027bab1ad742cbab
Xsitsu <jacoblocke21@live.com>
2018-12-04 13:58:41 -0800

    Added moon controller, basic empty views, and updated _Layout navbar to include them.

---
+++ Controllers/MoonController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EVE_Moon_Map.Controllers
+{
+    public class MoonController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Search()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Search(int v)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(int v)
+        {
+            return View();
+        }
+    }
+}

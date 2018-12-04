818bdc2539d0e648cded7d1b9f4d3e2d813ecb79
Xsitsu <jacoblocke21@live.com>
2018-12-04 05:08:31 -0800

    Created project and added some basic stuff.

---
+++ Areas/Identity/Data/EVE_Moon_MapContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EVE_Moon_Map.Models
+{
+    public class EVE_Moon_MapContext : IdentityDbContext<IdentityUser>
+    {
+        public EVE_Moon_MapContext(DbContextOptions<EVE_Moon_MapContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EVE_Moon_Map.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EVE_Moon_Map.Areas.Identity.IdentityHostingStartup))]
+namespace EVE_Moon_Map.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EVE_Moon_MapContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EVE_Moon_MapContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<EVE_Moon_MapContext>();
+            });
+        }
+    }
+}
